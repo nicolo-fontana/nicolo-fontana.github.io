f4d6dd1648c34598ee20d45c9b5d3aa7e81ef63c
nicolo-fontana <nicolo.nf.fontana@gmail.com>
2025-02-01 18:09:40 +0100

    Continued hello page

--- src/pages/HelloPage/HelloPage.tsx
+++ src/pages/HelloPage/HelloPage.tsx
@@ -9,37 +9,41 @@ const HelloPage: React.FC<HelloPageInterface> = () => {
   console.log(welcomeMessage);
 
   return (
-    <div className="d-flex justify-content-center align-items-center">
+    <div className="hello-page d-flex justify-content-center align-items-center position-relative">
+      <img
+        className="hello-page__bg-blurs position-absolute top-0"
+        src={BackgroundBlurs}
+        alt=""
+      />
       <div>
-        <div className="pb-5">
-          <p className="clr-secondary-400 theme-text-body m-0">Hi all. I am</p>
-          <p className="clr-secondary-400 theme-text-title m-0 lh-1">
+        <div className="pb-5 fade-in">
+          <div className="clr-secondary-400 theme-text-body m-0">
+            Hi all. I am
+          </div>
+          <div className="clr-secondary-400 theme-text-title m-0 lh-1">
             Nicolò Fontana
-          </p>
-          <p className="clr-secondary-300 theme-text-subtitle m-0 lh-1">
+          </div>
+          <div className="clr-secondary-300 theme-text-subtitle m-0 lh-1">
             &gt; Front-end developer
-          </p>
+          </div>
         </div>
-        <div className="pt-5">
-          <p className="clr-secondary-100 m-0 theme-text-body-small">
+        <div className="pt-5 d-flex flex-column align-items-start">
+          <div className="clr-secondary-100 theme-text-body-small typewriter">
             // Say hello to the user
-          </p>
-          <p className="m-0 theme-text-body-small clr-white">
+          </div>
+          <div className="m-0 theme-text-body-small clr-white typewriter typewriter-delay-1">
             <span className="clr-secondary-300">const</span>{" "}
             <span className="clr-gradient-end">welcomeMessage</span> ={" "}
             <span className="clr-accent-300">“Welcome to my portfolio”</span>;
-          </p>
-          <p className="m-0 theme-text-body-small clr-white">
+          </div>
+          <div className="m-0 theme-text-body-small clr-white typewriter typewriter-delay-2">
             <span className="clr-secondary-100">console</span>.
             <span className="clr-accent-100">log</span>
             <span className="clr-accent-400">(</span>
             <span className="clr-gradient-end">welcomeMessage</span>
             <span className="clr-accent-400">)</span>;
-          </p>
+          </div>
         </div>
-      </div>
-      <div>
-        <img className="hello-page__bg-blurs" src={BackgroundBlurs} alt="" />
       </div>
     </div>
   );
